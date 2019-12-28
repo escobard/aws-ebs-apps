f76dfa274b10c5944e79562aaff1df0cbf3c7768
Dan <escobard90@gmail.com>
2019-12-27 18:18:43 -0700

    section 11 - feat: ready for non nginx prod routing

--- ui/src/constants/index.js
+++ ui/src/constants/index.js
@@ -19,7 +19,7 @@ const environment =
   process.env.NODE_ENV === "production" ? "gcp" : "development";
 
 // expand in the future with more variants
-const apiRouteController = process.env.REACT_APP_DOCKER_VARIANT === "nginx_routing" ? "/api" : "http://localhost:4000" ;
+const apiRouteController = "http://localhost:4000";
 
 const apiRoot =
   environment === "gcp"
